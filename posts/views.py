db7a87d5d92d6c1b019944ae62de2caa03b7989a
sjahn <asj214@naver.com>
2020-12-30 14:37:36 +0900

    authentication, renderer, query set

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from rest_framework import status, viewsets
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.permissions import (
     AllowAny,
     IsAuthenticated,
@@ -12,13 +12,13 @@ from .serializers import PostSerializer
 
 
 class PostApiView(viewsets.ModelViewSet):
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
     serializer_class = PostSerializer
     renderer_classes = (PostRenderer,)
     queryset = Post.objects.prefetch_related('user')
 
     def list(self, request):
-        serializer_context = {"request": request}
+        serializer_context = {'request': request}
 
         page = self.paginate_queryset(self.queryset)
         serializer = self.serializer_class(page, context=serializer_context, many=True)
@@ -26,10 +26,10 @@ class PostApiView(viewsets.ModelViewSet):
         return self.get_paginated_response(serializer.data)
     
     def create(self, request):
-        serializer_context = {"user": request.user, "request": request}
+        serializer_context = {'user': request.user, 'request': request}
         serializer_data = {
-            "title": request.data.get("title"),
-            "body": request.data.get("body"),
+            'title': request.data.get('title'),
+            'body': request.data.get('body'),
         }
 
         serializer = self.serializer_class(
@@ -39,3 +39,35 @@ class PostApiView(viewsets.ModelViewSet):
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        serializer_context = {'request': request}
+
+        try:
+            serializer_instance = self.queryset.get(pk=pk)
+        except Post.DoesNotExist:
+            raise NotFound('Does not exist.')
+
+        serializer_data = request.data
+
+        serializer = self.serializer_class(
+            serializer_instance, 
+            context=serializer_context,
+            data=serializer_data, 
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def destroy(self, request, pk=None):
+
+        try:
+            post = self.queryset.get(pk=pk)
+        except Post.DoesNotExist:
+            raise NotFound('Does not exist.')
+    
+        post.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- system/renderers.py
+++ system/renderers.py
@@ -9,7 +9,9 @@ class BaseRenderer(JSONRenderer):
     pagination_object_count = 'count'
 
     def render(self, data, media_type=None, renderer_context=None):
-        if data.get('results', None) is not None:
+        if data is None:
+            return None
+        elif data.get('results', None) is not None:
             return json.dumps({
                 self.pagination_object_label: data['results'],
                 self.pagination_count_label: data['count']
@@ -20,7 +22,7 @@ class BaseRenderer(JSONRenderer):
         # the default JSONRenderer to handle rendering errors, so we need to
         # check for this case.
         elif data.get('errors', None) is not None:
-            return super(ConduitJSONRenderer, self).render(data)
+            return super(BaseRenderer, self).render(data)
         else:
             return json.dumps({
                 self.object_label: data
