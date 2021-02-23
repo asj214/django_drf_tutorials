0b4a9b8855d922fe0046611ce12134663cdf4560
sjahn <asj214@naver.com>
2021-02-23 23:23:24 +0900

    use redis example code

--- apps/views/examples.py
+++ apps/views/examples.py
@@ -10,7 +10,7 @@ from configs.utils import (
     fetchone,
     fetchall,
 )
-from configs.redis_connect import redis
+from configs.redis_connect import r
 from apps.serializers import ArticleSerializer
 
 
@@ -35,10 +35,73 @@ class AesCryptoDecryptView(APIView):
 class RedisView(APIView):
     permission_classes = (AllowAny,)
 
+    key = 'user:actions:likes'
+    count_key = 'user:actions:likes_cnt'
+    hkey = 'user:info'
+    hmkey = 'user:info:all'
+
+    def get_keys(self, user_id):
+        return {
+            'key': '{0}:{1}'.format(self.key, user_id),
+            'count_key': '{0}:{1}'.format(self.count_key, user_id),
+            'hkey': '{0}:{1}'.format(self.hkey, user_id),
+            'hmkey': '{0}:{1}'.format(self.hmkey, user_id),
+        }
+
     def post(self, request):
-        # redis.hset('ktown4u:authentications', 'token', )
-        # return Response({'foo': redis.hget('ktown4u:authentications', 'token')})
-        return Response()
+
+        user_id = request.data.get('user_id', 1)
+        keys = self.get_keys(user_id)
+        key = keys.get('key')
+        count_key = keys.get('count_key')
+        hkey = keys.get('hkey')
+        hmkey = keys.get('hmkey')
+
+        # list
+        if r.exists(key):
+            r.delete(key)
+
+        if not r.exists(count_key):
+            r.set(count_key, 0)
+
+        for i in range(10):
+            r.lpush(key, i)
+            r.incr(count_key)
+
+        # hash
+        r.hset(hkey, 'id', 1)
+        r.hset(hkey, 'name', 'sjahn')
+        r.hset(hkey, 'email', 'sjahn@ktown4u.com')
+
+        # all
+        r.hmset(hmkey, {'id': 1, 'name': 'sjahn', 'email': 'sjahn@ktown4u.com'})
+
+        res = {
+            key: r.lrange(key, 0, r.llen(key)),
+            count_key: r.get(count_key),
+            hkey: {
+                'part': {
+                    'id': r.hget(hkey, 'id'),
+                    'name': r.hget(hkey, 'name'),
+                    'email': r.hget(hkey, 'email'),
+                },
+                'all': r.hgetall(hkey)
+            },
+            hmkey: r.hgetall(hmkey)
+        }
+
+        return Response(res)
+
+    def delete(self, request):
+
+        user_id = request.data.get('user_id', 1)
+        units = self.get_keys(user_id)
+
+        for key, value in units.items():
+            if r.exists(value):
+                r.delete(value)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class RawQueryCRUDViewset(viewsets.ViewSet):
--- configs/redis_connect.py
+++ configs/redis_connect.py
@@ -1,4 +1,5 @@
 from redis import Redis
+from rest_framework import exceptions
 from .settings import (
     REDIS_HOST,
     REDIS_PORT,
@@ -6,4 +7,7 @@ from .settings import (
 )
 
 
-redis = Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+try:
+    r = Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
+except redis.RedisError as exception:
+    raise RedisKeyedVectorException(exception)
